a59ab92b6211e9f636f84448bd6b1392c1ae9586
Gng <gjhsmiles@gmail.com>
2019-03-10 01:59:35 +0800

    remove node_modules folder

--- frontend/node_modules/antd/es/form/context.d.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from 'create-react-context';
-import { ColProps } from '../grid/col';
-export interface FormContextProps {
-    vertical: boolean;
-    labelCol?: ColProps;
-    wrapperCol?: ColProps;
-}
-export declare const FormContext: Context<FormContextProps>;
