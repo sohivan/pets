0681131b4b27624e122a372aaafc0ea54016629b
sohivan <ivansoh_94@hotmail.com>
2019-03-11 02:51:57 +0800

    added new tables

--- pets.sql
+++ pets.sql
@@ -7,3 +7,35 @@ CREATE TABLE Users (
 	password 		text not null
 );
 
+DROP TABLE if exists PetOwners;
+CREATE TABLE PetOwners (
+	id 				SERIAL,
+	description 	text not null,
+	numofpets		integer not null,
+	name			text not null
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES Users
+);
+
+DROP TABLE if exists Pets;
+CREATE TABLE Pets (
+	pid				SERIAL,
+	size			integer not null,
+	age				integer not null,
+	breed			text not null,
+	name			text not null,
+	typeofpet		text not null,
+	id				SERIAL
+	PRIMARY KEY (pid),
+	FOREIGN KEY (id) REFERENCES PetOwners
+);
+
+DROP TABLE if exists Homes;
+CREATE TABLE Homes (
+	address			text,
+	area			integer not null,
+	type 			text not null,
+	id				SERIAL
+	PRIMARY KEY (address),
+	FOREIGN KEY (id) REFERENCES Users
+);
