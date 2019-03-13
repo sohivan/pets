e3b03e7ca6176b9d8220a22c97cfc1e5e93ef449
sohivan <ivansoh_94@hotmail.com>
2019-03-13 10:55:11 +0800

    added data for sql tables

--- pets.sql
+++ pets.sql
@@ -8,18 +8,18 @@ CREATE TABLE Users (
 
 
 INSERT INTO Users (id, name, email, password) VALUES
-(255,'Renee','tempor.bibendum@vel.com','BUZ77LFA2JY');
-(810,'Paki','vehicula.aliquet@Aeneaneuismod.ca','BVJ15SCT9SU');
-(525,'Minerva','tincidunt@facilisismagnatellus.net','CFE88RYX6BJ');
-(332,'Patrick','ipsum.leo.elementum@dignissimMaecenas.co.uk','RKS50SHP9GG');
-(548,'Iona','venenatis.vel@Aeneaneuismod.org','WSU52DBI7QE');
-(489,'Karyn','erat.vitae@disparturientmontes.ca','MHK80XPP7CK');
-(932,'Solomon','quam.quis.diam@molestie.co.uk','QGT17PZZ2IJ');
-(508,'Octavius','Phasellus@Aliquamadipiscing.ca','LIO34VZC2UV');
-(673,'Violet','Quisque.tincidunt.pede@vitaenibh.com','UFK59FOD0YT');
-(640,'Jolie','sem.ut@faucibusorci.ca','ZZT15HBV5XT');
-(725,'Heather','nunc.risus@acturpis.com','JBK85ODN5PL');
-(941,'Yoshio','Nulla.eget.metus@magnis.net','BTD17WGQ1UB');
+(255,'Renee','tempor.bibendum@vel.com','BUZ77LFA2JY'),
+(810,'Paki','vehicula.aliquet@Aeneaneuismod.ca','BVJ15SCT9SU'),
+(525,'Minerva','tincidunt@facilisismagnatellus.net','CFE88RYX6BJ'),
+(332,'Patrick','ipsum.leo.elementum@dignissimMaecenas.co.uk','RKS50SHP9GG'),
+(548,'Iona','venenatis.vel@Aeneaneuismod.org','WSU52DBI7QE'),
+(489,'Karyn','erat.vitae@disparturientmontes.ca','MHK80XPP7CK'),
+(932,'Solomon','quam.quis.diam@molestie.co.uk','QGT17PZZ2IJ'),
+(508,'Octavius','Phasellus@Aliquamadipiscing.ca','LIO34VZC2UV'),
+(673,'Violet','Quisque.tincidunt.pede@vitaenibh.com','UFK59FOD0YT'),
+(640,'Jolie','sem.ut@faucibusorci.ca','ZZT15HBV5XT'),
+(725,'Heather','nunc.risus@acturpis.com','JBK85ODN5PL'),
+(941,'Yoshio','Nulla.eget.metus@magnis.net','BTD17WGQ1UB'),
 (950,'Keane','dignissim.Maecenas@interdumenimnon.org','STX76YFI9BG');
 
 
@@ -28,24 +28,24 @@ CREATE TABLE PetOwners (
 	oid				SERIAL not null REFERENCES Users(id),
 	name			VARCHAR(100) not null REFERENCES Users(name)
 	description 	text not null,
-	numofpets		smallint(100) not null,
+	numofpets		smallint  not null,
 	PRIMARY KEY (oid)
 );
 
 INSERT INTO PetOwners (oid, name, description, numofpets) VALUES
-(255,'Renee','aaaaaa',5);
-(810,'Paki','aaaaaaa',3);
-(525,'Minerva','aaaaaaa',2);
-(332,'Patrick','aaaaaaaa',1);
-(548,'Iona','aaaaaaaa',2);
+(255,'Renee','aaaaaa',5),
+(810,'Paki','aaaaaaa',3),
+(525,'Minerva','aaaaaaa',2),
+(332,'Patrick','aaaaaaaa',1),
+(548,'Iona','aaaaaaaa',2),
 (489,'Karyn','aaaaaaaa',1);
 
 DROP TABLE if exists Pets;
 CREATE TABLE Pets (
 	PetID			SERIAL primary key,
 	name			VARCHAR(100) not null,
-	weight			smallint(200) not null,
-	age				smallint(200) not null,
+	weight			smallint not null,
+	age				smallint not null,
 	breed			VARCHAR(100) not null,
 	name			VARCHAR(100) not null,
 	typeofpet		VARCHAR(100) not null,
@@ -54,19 +54,19 @@ CREATE TABLE Pets (
 );
 
 INSERT INTO Pets (PetID, name, weight, age, breed, typeofpet, oid) VALUES
-(1166,'Hop','2.7556552703','3.8885158019','Poodle','dog',255);
-(1177,'Rebecca','2.9118732289','4.7711281473','Husky','dog',255);
-(1379,'Xandra','2.9936743322','2.8173021695','Shitzu','dog',255);
-(3590,'Lana','2.4297842701','4.9697348125','Corgi','dog',255);
-(9176,'Zeph','2.9718911422','5.3035622243','Husky','dog',255);
-(5664,'Leilani','3.8819051889','3.5992421957','Poodle','dog',810);
-(9056,'Kaye','2.3296668095','3.4407839171','Terrier','dog',810);
-(5014,'Sydney','2.6870185783','4.629419826','Corgi','dog',810);
-(8600,'Jack','3.2861363197','3.5036991688','Husky','dog',525);
-(2594,'Constance','2.9453932727','3.8632163484','Poodle','dog',525);
-(9108,'Dalton','3.6034310875','3.3706082931','Husky','dog',332);
-(4152,'Maxine','2.6234841657','6.066543142','Corgi','dog',548);
-(6373,'Walter','4.3843708009','5.2702815954','Shitzu','dog',548);
+(1166,'Hop','2.7556552703','3.8885158019','Poodle','dog',255),
+(1177,'Rebecca','2.9118732289','4.7711281473','Husky','dog',255),
+(1379,'Xandra','2.9936743322','2.8173021695','Shitzu','dog',255),
+(3590,'Lana','2.4297842701','4.9697348125','Corgi','dog',255),
+(9176,'Zeph','2.9718911422','5.3035622243','Husky','dog',255),
+(5664,'Leilani','3.8819051889','3.5992421957','Poodle','dog',810),
+(9056,'Kaye','2.3296668095','3.4407839171','Terrier','dog',810),
+(5014,'Sydney','2.6870185783','4.629419826','Corgi','dog',810),
+(8600,'Jack','3.2861363197','3.5036991688','Husky','dog',525),
+(2594,'Constance','2.9453932727','3.8632163484','Poodle','dog',525),
+(9108,'Dalton','3.6034310875','3.3706082931','Husky','dog',332),
+(4152,'Maxine','2.6234841657','6.066543142','Corgi','dog',548),
+(6373,'Walter','4.3843708009','5.2702815954','Shitzu','dog',548),
 (8627,'Joel','3.9528381721','3.9515375944','Corgi','dog',489);
 
 
@@ -86,7 +86,7 @@ CREATE TABLE Bid (
 	BidEndDate		date not null,
 	BidID			SERIAL primary key,
 	BidTimestamp	timestamp not null,
-	BidAmount		smallint(1000) not null,
+	BidAmount		smallint not null,
 	PetID 			SERIAL not null REFERENCES Pets(PetID),
 	PetOnwner		SERIAL not null REFERENCES PetOwners(oid),
 	BidderID		SERIAL not null REFERENCES CareTaker(cid),
@@ -125,12 +125,12 @@ CREATE TABLE CareTaker (
 );
 
 INSERT INTO CareTaker (cid, name, Preference, Description) VALUES
-(932,'Solomon','aaaaaaaa','bbbb');
-(508,'Octavius','aaaaaaaa','bbbbb');
-(673,'Violet','aaaaaaa','bbbbb');
-(640,'Jolie','aaaaaaa','bbbbb');
-(725,'Heather','aaaaaaa','bbbbb');
-(941,'Yoshio','aaaaaaa','bbbbbb');
+(932,'Solomon','aaaaaaaa','bbbb'),
+(508,'Octavius','aaaaaaaa','bbbbb'),
+(673,'Violet','aaaaaaa','bbbbb'),
+(640,'Jolie','aaaaaaa','bbbbb'),
+(725,'Heather','aaaaaaa','bbbbb'),
+(941,'Yoshio','aaaaaaa','bbbbbb'),
 (950,'Keane','aaaaaaa','bbbbb');
 
 DROP TABLE if exists Review;
@@ -139,9 +139,9 @@ CREATE TABLE Review (
 	RevieweeID		SERIAL not null REFERENCES Users(id),
 	ReviewerID		SERIAL not null REFERENCES Users(id),
 	BookingID		SERIAL not null REFERENCES History(BookingID),
-	Overall			smallint(10),
-	Convenience		smallint(10),
-	Friendliness	smallint(10)
+	Overall			smallint,
+	Convenience		smallint,
+	Friendliness	smallint
 );
 
 DROP TABLE if exists Services;
@@ -149,28 +149,28 @@ CREATE TABLE Services (
 	Service 		VARCHAR(100) not null,
 	StartDate 		DATE not null,
 	EndDate 		DATE not null,
-	Rate			smallint(200) not null,
+	Rate			smallint not null,
 	cid				SERIAL not null REFERENCES CareTaker(cid)
 );
 
 INSERT INTO Services (Service, StartDate, EndDate, Rate, cid) VALUES
-("Overnight","30-01-2019","23-02-2019",81,932);
-("Overnight","24-01-2019","25-01-2019",50,508);
-("Washing","23-01-2019","20-02-2019",92,932);
-("Walking","25-01-2019","20-02-2019",87,932);
-("Walking","30-01-2019","25-01-2019",90,508);
-("Overnight","30-01-2019","23-02-2019",93,950);
-("Walking","22-01-2019","20-02-2019",87,950);
-("Feeding","23-01-2019","20-02-2019",88,508);
-("Overnight","22-01-2019","23-02-2019",61,941);
-("Walking","30-01-2019","27-02-2019",71,941);
-("Vet Visitation","22-01-2019","10-02-2019",67,950);
-("Vet Visitation","27-01-2019","20-02-2019",98,950);
-("Feeding","10-01-2019","27-02-2019",57,725);
-("Washing","20-01-2019","26-01-2019",99,725);
-("Vet Visitation","10-01-2019","23-02-2019",96,725);
-("Feeding","24-01-2019","24-02-2019",81,640);
-("Overnight","20-01-2019","24-02-2019",51,640);
+("Overnight","30-01-2019","23-02-2019",81,932),
+("Overnight","24-01-2019","25-01-2019",50,508),
+("Washing","23-01-2019","20-02-2019",92,932),
+("Walking","25-01-2019","20-02-2019",87,932),
+("Walking","30-01-2019","25-01-2019",90,508),
+("Overnight","30-01-2019","23-02-2019",93,950),
+("Walking","22-01-2019","20-02-2019",87,950),
+("Feeding","23-01-2019","20-02-2019",88,508),
+("Overnight","22-01-2019","23-02-2019",61,941),
+("Walking","30-01-2019","27-02-2019",71,941),
+("Vet Visitation","22-01-2019","10-02-2019",67,950),
+("Vet Visitation","27-01-2019","20-02-2019",98,950),
+("Feeding","10-01-2019","27-02-2019",57,725),
+("Washing","20-01-2019","26-01-2019",99,725),
+("Vet Visitation","10-01-2019","23-02-2019",96,725),
+("Feeding","24-01-2019","24-02-2019",81,640),
+("Overnight","20-01-2019","24-02-2019",51,640),
 ("Walking","25-01-2019","26-01-2019",53,640);
 
 
