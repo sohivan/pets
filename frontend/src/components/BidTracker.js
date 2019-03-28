3a90f7f41900b652a996dc371a142dff637a4f31
sohivan <ivansoh_94@hotmail.com>
2019-03-28 11:36:23 +0800

    modified Bid Tracker

---
+++ frontend/src/components/BidTracker.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Upload, Icon, Modal,Form, Input, Cascader, Select, Slider, DatePicker, InputNumber, Row, Col, Checkbox, Button, Card, Rate, Pagination} from 'antd';
+import './BidTracker.css';
+
+export class BidTracker extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bid Tracker</h1>
+      </div>
+    )
+  }
+}
+
+export default BidTracker
