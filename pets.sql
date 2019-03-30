dbc1e6bbb961ff7b5d26087f81f0f1bb0120ce74
sohivan <ivansoh_94@hotmail.com>
2019-03-30 21:23:00 +0800

    added timestamp for users

--- pets.sql
+++ pets.sql
@@ -8,6 +8,7 @@ DROP TABLE if exists CareTaker cascade;
 DROP TABLE if exists Services cascade;
 DROP TABLE if exists admins cascade;
 
+
 CREATE TABLE admins (
 	id 			SERIAL PRIMARY KEY
 );
@@ -23,30 +24,30 @@ CREATE TABLE users (
 );
 
 
-INSERT INTO users (id, name, email, password) VALUES
-(255,'Renee','tempor.bibendum@vel.com','BUZ77LFA2JY'),
-(810,'Paki','vehicula.aliquet@Aeneaneuismod.ca','BVJ15SCT9SU'),
-(525,'Minerva','tincidunt@facilisismagnatellus.net','CFE88RYX6BJ'),
-(332,'Patrick','ipsum.leo.elementum@dignissimMaecenas.co.uk','RKS50SHP9GG'),
-(548,'Iona','venenatis.vel@Aeneaneuismod.org','WSU52DBI7QE'),
-(489,'Karyn','erat.vitae@disparturientmontes.ca','MHK80XPP7CK'),
-(932,'Solomon','quam.quis.diam@molestie.co.uk','QGT17PZZ2IJ'),
-(508,'Octavius','Phasellus@Aliquamadipiscing.ca','LIO34VZC2UV'),
-(673,'Violet','Quisque.tincidunt.pede@vitaenibh.com','UFK59FOD0YT'),
-(640,'Jolie','sem.ut@faucibusorci.ca','ZZT15HBV5XT'),
-(725,'Heather','nunc.risus@acturpis.com','JBK85ODN5PL'),
-(941,'Yoshio','Nulla.eget.metus@magnis.net','BTD17WGQ1UB'),
-(950,'Keane','dignissim.Maecenas@interdumenimnon.org','STX76YFI9BG'),
- (123, 'W', 'hello123@gmail.com', '12341234');
+INSERT INTO users (id, name, email, password,lastlogintimestamp) VALUES
+(255,'Renee','tempor.bibendum@vel.com','BUZ77LFA2JY','1990-09-26 00:16:06'),
+(810,'Paki','vehicula.aliquet@Aeneaneuismod.ca','BVJ15SCT9SU','1990-09-26 00:16:06'),
+(525,'Minerva','tincidunt@facilisismagnatellus.net','CFE88RYX6BJ','1990-09-26 00:16:06'),
+(332,'Patrick','ipsum.leo.elementum@dignissimMaecenas.co.uk','RKS50SHP9GG','1990-09-26 00:16:06'),
+(548,'Iona','venenatis.vel@Aeneaneuismod.org','WSU52DBI7QE','1990-09-26 00:16:06'),
+(489,'Karyn','erat.vitae@disparturientmontes.ca','MHK80XPP7CK','1990-09-26 00:16:06'),
+(932,'Solomon','quam.quis.diam@molestie.co.uk','QGT17PZZ2IJ','1990-09-26 00:16:06'),
+(508,'Octavius','Phasellus@Aliquamadipiscing.ca','LIO34VZC2UV','1990-09-26 00:16:06'),
+(673,'Violet','Quisque.tincidunt.pede@vitaenibh.com','UFK59FOD0YT','1990-09-26 00:16:06'),
+(640,'Jolie','sem.ut@faucibusorci.ca','ZZT15HBV5XT','1990-09-26 00:16:06'),
+(725,'Heather','nunc.risus@acturpis.com','JBK85ODN5PL','1990-09-26 00:16:06'),
+(941,'Yoshio','Nulla.eget.metus@magnis.net','BTD17WGQ1UB','1990-09-26 00:16:06'),
+(950,'Keane','dignissim.Maecenas@interdumenimnon.org','STX76YFI9BG','1990-09-26 00:16:06'),
+(123, 'W', 'hello123@gmail.com', '12341234','1990-09-26 00:16:06');
 
 
 CREATE TABLE PetOwners (
 	oid				serial primary key,
 	owner_name		text not null,
 	description 	text not null,
-	numofpets		smallint  not null,
 	foreign key (oid, owner_name) references users(id,name)
 );
+
 
 INSERT INTO PetOwners (oid, owner_name, description, numofpets) VALUES
 (255,'Renee','aaaaaa',5),
@@ -58,19 +59,20 @@ INSERT INTO PetOwners (oid, owner_name, description, numofpets) VALUES
 
 
 CREATE TABLE Pets (
-	PetID			SERIAL primary key,
-	name			VARCHAR(100) not null,
-	weight			smallint not null,
-	age				smallint not null,
-	breed			VARCHAR(100) not null,
-	typeofpet		VARCHAR(100) not null,
-	gender			VARCHAR (100) not null, 
-	descrip			text,
-	med				text,
-	oid				SERIAL not null REFERENCES PetOwners(oid)
+	PetID				SERIAL primary key,
+	name				VARCHAR(100) not null,
+	weight				smallint not null,
+	age					smallint not null,
+	breed				VARCHAR(100) not null,
+	PetType				VARCHAR(100) not null,
+	gender				VARCHAR (100) not null, 
+	description			text,
+	medical_conditions	text,
+	oid					SERIAL not null REFERENCES PetOwners(oid)
 );
 
-INSERT INTO Pets (PetID, name, weight, age, breed, typeofpet, gender, descrip, med, oid) VALUES
+
+INSERT INTO Pets (PetID, name, weight, age, breed, PetType, gender, description, medical_conditions, oid) VALUES
 (1166,'Hop',2,3,'Poodle','dog', 'male', 'hop likes to chew things', 'teething', 255),
 (1177,'Rebecca',3,4,'Husky','dog', 'female', 'bec is socialable dog and loves hanging out with other animals', 'none', 255),
 (1379,'Xandra',1,5,'Shitzu','dog', 'female', 'xandra loves sports!', null, 255),
@@ -87,7 +89,6 @@ INSERT INTO Pets (PetID, name, weight, age, breed, typeofpet, gender, descrip, m
 (8627,'Joel',2,6,'Corgi','dog','male', 'joel loves to swim', null,  489);
 
 
-
 CREATE TABLE Homes (
 	address			text not null,
 	area			integer not null,
@@ -97,8 +98,6 @@ CREATE TABLE Homes (
 );
 
 
-
-
 CREATE TABLE CareTaker (
 	cid				SERIAL unique not null,
 	name 			text not null,
@@ -106,13 +105,14 @@ CREATE TABLE CareTaker (
 	PetSize			smallint not null,
 	housingOptions	smallint not null,
 	miscOptions		text,
-	Description 	text not null,
+	description 	text not null,
 	NumOfPet		smallint not null, 
 	-- address			text not null REFERENCES Homes(address)
 	foreign key (cid, name) references users(id, name)
 );
 
-INSERT INTO CareTaker (cid, name, PetType, PetSize, housingOptions, miscOptions, Description, NumOfPet) VALUES
+
+INSERT INTO CareTaker (cid, name, PetType, PetSize, housingOptions, miscOptions, description, NumOfPet) VALUES
 (932,'Solomon','Dog', 1,0,3,'likes to eat', 1),
 (508,'Octavius','Dog', 1,1,2,'likes to sleep', 2),
 (673,'Violet','Dog', 1,2,1,'vegetarian',1),
