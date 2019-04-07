d92f7308290bf7cba5c3e290af922d2ec5080d37
Wyin Kok <wyin.kwy@gmail.com>
2019-04-07 21:50:02 +0800

    Add admin UI

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './Admin.css';
-import {Form, Input, Button, Icon, Row, Col } from 'antd';
+import { Table, Divider, Tag, Input, Button, Popconfirm, Form } from 'antd';
+
+const { Column, ColumnGroup } = Table;
+
 
 class Admin extends Component {
   constructor(props) {
@@ -10,7 +13,44 @@ class Admin extends Component {
   render() {
       return (
         <div>
-        <h1>List of inactive users</h1>
+        <h1 className="admin-title">List of inactive bids</h1>
+        <Table >
+        <Column
+          title="Bid Date"
+          dataIndex="bidtimestamp"
+          key="bidtimestamp"
+        />
+        <Column
+          title="Pet Owner"
+          dataIndex="owner_name"
+          key="owner_name"
+        />
+        <Column
+          title="Pet Name"
+          dataIndex="name"
+          key="name"
+        />
+        <Column
+        title="Type"
+        dataIndex="typeofpet"
+        key="typeofpet"
+        />
+        <Column
+        title="Service"
+        dataIndex="service"
+        key="service"
+        />
+        <Column
+        title="Start Date"
+        dataIndex="bidstartdate"
+        key="bidstartdate"
+        />
+        <Column
+        title="End Date"
+        dataIndex="bidenddate"
+        key="bidenddate"
+        />
+        </Table>
         </div>
   );
 
