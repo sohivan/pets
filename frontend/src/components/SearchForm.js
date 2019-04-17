d8b086590fd441a30c699ac8380032572c95db8b
Wyin Kok <wyin.kwy@gmail.com>
2019-04-18 00:40:19 +0800

    Pull dynamic images from database

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -152,7 +152,7 @@ displayResults = (results) => {
               className="results-card"
                bordered={false}
                style={{ width: 240 }}
-               cover={<img alt="example" src="https://i.pinimg.com/originals/74/6d/ab/746dab017f2d7f3c5da3aa7b1995d706.jpg" />}
+               cover={<img alt="example" src={array[i].image} />}
 
               >
              <Card.Meta
@@ -178,7 +178,7 @@ displayResults = (results) => {
                bordered={false}
                style={{ width: 240 }}
                onClick={() => this.cardClick(array[i+1].cid)}
-               cover={<img alt="example" src="https://us.123rf.com/450wm/warrengoldswain/warrengoldswain1603/warrengoldswain160300008/54380987-hipster-man-petting-and-rubbing-his-dog-loving-affection-relationship-bond-between-owner-and-pet.jpg?ver=6" />}>
+               cover={<img alt="example" src={array[i].image} />}>
              <Card.Meta
                title= {
                  <div>
