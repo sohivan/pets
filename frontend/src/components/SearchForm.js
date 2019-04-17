caf02dd637f35d745ce2fc22cadba63c775f0725
Gng <gjhsmiles@gmail.com>
2019-04-18 00:13:36 +0800

    fix filter page

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -3,6 +3,8 @@ import {Form, Select, Slider, DatePicker, InputNumber, Row, Col, Checkbox, Butto
 import AlgoliaPlaces from 'algolia-places-react';
 import './SearchForm.css';
 import { NavLink , withRouter } from 'react-router-dom';
+import moment from 'moment';
+
 
 
 const Option = Select.Option;
@@ -42,37 +44,81 @@ class SearchForm extends Component {
 
 
 class Search extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-   if (this.props.location.pathname == nextProps.location.pathname && JSON.stringify({a: this.state.currentResultsDisplay}) === JSON.stringify({a: nextProps.currentResultsDisplay})) {
-     return false;
-   } else {
-     return true;
-   }
- }
+ //  shouldComponentUpdate(nextProps, nextState) {
+ //   if (this.props.location.pathname == nextProps.location.pathname && JSON.stringify({a: this.state.currentResultsDisplay}) === JSON.stringify({a: nextProps.currentResultsDisplay})) {
+ //     console.log("its falsee");
+ //     return false;
+ //   } else {
+ //     console.log("its true");
+ //
+ //     return true;
+ //   }
+ // }
 
   constructor(props) {
+    console.log(props);
+    console.log(props.searchFilters.housingopt);
     super(props);
     this.myRef = React.createRef();
     this.state = {
      marks: {
         0: 10,
-        100: 100
+        100: props.searchFilters.marks
       },
       location: {},
       results:[],
       totalResultsDisplay:[],
       currentResultsDisplay: props.currentResultsDisplay,
       current: 1,
-      service: 'Pet Boarding',
-      pettype: 'Dog',
-      petsize: 1,
-      numofpet: 1,
-      housingopt: 0,
-      miscopt: 0,
-      startdate: '',
-      enddate: '',
-      filter: 0
-  };
+      service: props.searchFilters.service,
+      pettype: props.searchFilters.pettype,
+      petsize: props.searchFilters.petsize,
+      petsizeDisp: "Small: 0 - 5kg",
+      numofpet: props.searchFilters.numofpet,
+      housingopt: props.searchFilters.housingopt,
+      housingoptDisp: [],
+      miscopt: props.searchFilters.miscopt,
+      miscoptDisp: [],
+      startdate: props.searchFilters.startdate,
+      enddate: props.searchFilters.enddate,
+      filter: props.searchFilters.filter,
+      dates: null,
+    };
+
+    if(props.searchFilters.petsize ==1) {
+      this.state.petsizeDisp = "Small: 0 - 5kg"
+    } else if(props.searchFilters.petsize ==2){
+         this.state.petsizeDisp = "Medium: 6 - 15kg"
+    } else if(props.searchFilters.petsize == 3 ){
+        this.state.petsizeDisp = "Large: 16 - 45kg"
+   } else {
+       this.state.petsizeDisp = "Giant: > 45kg"
+   }
+
+   if(props.searchFilters.housingopt === 0){
+      this.state.housingoptDisp = [];
+    } else if(props.searchFilters.housingopt === 1){
+        this.state.housingoptDisp = ["Do not have other pets in their house"];
+   } else if(props.searchFilters.housingopt === 2){
+        this.state.housingoptDisp = ["Do not cage pets"];
+  } else {
+       this.state.housingoptDisp = ["Do not have other pets in their house", "Do not cage pets"];
+    }
+
+    if(props.searchFilters.miscopt === 0){
+       this.state.miscoptDisp = [];
+     } else if(props.searchFilters.miscopt === 1){
+         this.state.miscoptDisp = ["Takes care of one client at a time"];
+    } else if(props.searchFilters.miscopt === 2){
+         this.state.miscoptDisp = ["Pet First-Aid certified"];
+   } else {
+        this.state.miscoptDisp = ["Takes care of one client at a time", "Pet First-Aid certified"];
+     }
+
+   if (props.searchFilters.startdate!= null && props.searchFilters.enddate!= null ) {
+     this.state.dates =  [moment(props.searchFilters.startdate, 'YYYY-MM-DD'),moment(props.searchFilters.enddate, 'YYYY-MM-DD')]
+   }
+
   }
 
   onPageChange = (page) => {
@@ -124,7 +170,7 @@ class Search extends Component {
           results = data
           console.log(results)
           this.displayResults(results)
-         this.props.onSearchFilter(selection.service,selection.startdate, selection.enddate, this.state.currentResultsDisplay);
+         this.props.onSearchFilter(selection, this.state.currentResultsDisplay);
         })
 
       .catch(function(err) {
@@ -361,7 +407,7 @@ displayResults = (results) => {
               name="role"
               autoComplete="off"
               style={{ width: '100%', fontSize: '14px' }}
-              defaultValue={['Pet Boarding']}
+              defaultValue={[this.state.service]}
               onChange = {event => this.onServiceChange(event)}>
               {services.map(service => <Option key={service}>{service}</Option>)}
             </Select>
@@ -393,7 +439,7 @@ displayResults = (results) => {
           <Form.Item>
             <Slider
               marks={this.state.marks}
-              defaultValue={100}
+              defaultValue={this.state.marks[100]}
               tipFormatter={(value) => `$${value}`}
               onChange={this.onRateChange.bind(this)}/>
           </Form.Item>
@@ -404,7 +450,8 @@ displayResults = (results) => {
              <DatePicker.RangePicker
                 style={{ fontSize: '14px' }}
                 onChange={this.onDateChange.bind(this)}
-                placeholder={['Drop off', 'Pick up']}/>
+                placeholder={['Drop off', 'Pick up']}
+                defaultValue={this.state.dates}/>
           </Form.Item>
           <div className="slider-label">
             <p>Pet Type</p>
@@ -415,7 +462,7 @@ displayResults = (results) => {
               name="role"
               autoComplete="off"
               style={{ width: '100%', fontSize: '14px'  }}
-              defaultValue={['Dog']}
+              defaultValue={[this.state.pettype]}
               onChange = {event => this.onPetTypeChange(event)}>
               {pettype.map(pettype => <Option key={pettype}>{pettype}</Option>)}
             </Select>
@@ -429,7 +476,7 @@ displayResults = (results) => {
               name="role"
               autoComplete="off"
               style={{ width: '100%', fontSize: '14px' }}
-              defaultValue={['Small: 0 - 5kg']}
+              defaultValue={[this.state.petsizeDisp]}
               onChange={event => this.onPetSizeChange(event)}>
               {petsize.map(petsize => <Option key={petsize}>{petsize}</Option>)}
             </Select>
@@ -440,7 +487,7 @@ displayResults = (results) => {
           <InputNumber
               min={1}
               max={10}
-              defaultValue={1}
+              defaultValue={this.state.numofpet}
               style={{ width: '100%', marginBottom: '25px', fontSize: '14px' }}
               onChange={event => this.onPetNoChange(event)} />
           <div className="slider-label">
@@ -448,6 +495,7 @@ displayResults = (results) => {
           </div>
           <CheckboxGroup
             options={houseOptions}
+            defaultValue={this.state.housingoptDisp}
             onChange={event => this.onHousingChange(event)} />
             <br /><br />
           <div className="slider-label">
@@ -455,6 +503,7 @@ displayResults = (results) => {
           </div>
           <CheckboxGroup
             options={miscOptions}
+              defaultValue={this.state.miscoptDisp}
             onChange={event => this.onMiscChange(event)} />
             <br /><br />
           <Button className= "search-button" type="primary" htmlType="submit" onClick={this.handleSubmit.bind(this)}>
