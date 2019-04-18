0287451d7798a4dc98763969a549ed37fb73565c
Katherine <kat-sun@hotmail.com>
2019-04-18 11:49:18 +0800

    edit petprofile

--- frontend/src/components/PetProfile.js
+++ frontend/src/components/PetProfile.js
@@ -3,40 +3,35 @@ import { withRouter } from 'react-router-dom';
 import './PetProfile.css';
 import {Form, Input, Button, Icon, Row, Col } from 'antd';
 
-class PetProfile extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+
+
+function PetProfile({history, location}){
       return (
         <div>
-        <Button className="pet-profile-button" type="primary">
+        <Button className="pet-profile-button" type="primary" onClick={() => history.push("/my-user-profile")}>
             <Icon type="left" />Back to owner's profile</Button>
         <Row>
         <Col span={12}>
         <div className = "pet-profile-pic">
-        <img className = "pet-profile-img" src="https://upload.wikimedia.org/wikipedia/commons/8/86/Maltese_puppy.jpeg" alt="pet image1"/>
-        <img className = "pet-profile-img" src="https://i.pinimg.com/originals/07/ad/bb/07adbb578d159d2bde3eaaf89cbdcf14.jpg" alt="pet image2"/>
-        <img className = "pet-profile-img" src="https://irp-cdn.multiscreensite.com/c096e00e/dms3rep/multi/mobile/cute-maltese-dog%20(330%20x%20296)-min-330x296.jpg" alt="pet image3"/>
+        <img className = "pet-profile-img" src={location.state.im1} alt="pet image1"/>
+        <img className = "pet-profile-img" src={location.state.im2} alt="pet image2"/>
+        <img className = "pet-profile-img" src={location.state.im3} alt="pet image3"/>
         </div>
         </Col>
-
         <Col span={12}>
           <div className="pet-profile">
-          <h1 className="pet-profile-subtitle">Pet Name:</h1>
-          <h1 className="pet-profile-subtitle">Type:</h1>
-          <h1 className="pet-profile-subtitle">Breed:</h1>
-          <h1 className="pet-profile-subtitle">Size:</h1>
-          <h1 className="pet-profile-subtitle">Gender:</h1>
-          <h1 className="pet-profile-subtitle">Age:</h1>
-          <h1 className="pet-profile-subtitle">Description:</h1>
-          <h1 className="pet-profile-subtitle">Medical Condition/Dietary Requests: </h1>
+          <h1 className="pet-profile-subtitle"><b>Name:</b> {location.state.name}</h1>
+          <h1 className="pet-profile-subtitle"><b>Type:</b> {location.state.type}</h1>
+          <h1 className="pet-profile-subtitle"><b>Weight:</b> {location.state.weight}</h1>
+          <h1 className="pet-profile-subtitle"><b>Breed:</b> {location.state.breed}</h1>
+          <h1 className="pet-profile-subtitle"><b>Gender:</b> {location.state.gender}</h1>
+          <h1 className="pet-profile-subtitle"><b>Age:</b> {location.state.age}</h1>
+          <h1 className="pet-profile-subtitle"><b>Description:</b> {location.state.description}</h1>
+          <h1 className="pet-profile-subtitle"><b>Medical Condition/Dietary Requests:</b> {location.state.med}</h1>
           </div>
         </Col>
         </Row>
         </div>
   );
-
-}}
-
+}
 export default withRouter(PetProfile);
